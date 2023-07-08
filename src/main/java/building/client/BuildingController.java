33b89e139468cb51519315ca2fb07fb28f86c124
Laura Cattano <laura@Lauras-Air.fritz.box>
2023-07-08 23:12:28 +0100

    added missing logger in the client

--- src/main/java/building/client/BuildingController.java
+++ src/main/java/building/client/BuildingController.java
@@ -7,49 +7,56 @@ import building.BuildingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.logging.Logger;
+
 public class BuildingController {
-public static void main (String[] args) {
 
-	ManagedChannel channel = ManagedChannelBuilder.forAddress ("localhost", 50051).usePlaintext().build();
-	
-	BuildingServiceGrpc.BuildingServiceBlockingStub stub = BuildingServiceGrpc.newBlockingStub(channel);
-	
-	AdjustTemperatureRequest request = AdjustTemperatureRequest.newBuilder().setAreaId("Room1").setTemperature(22).build();
-	AdjustTemperatureResponse response = stub.adjustTemperature(request);
-	
-	//Printing adjust temperature response
-    System.out.println("Temperature adjustment was successful: " );
-	
-	
-	
-     
-     // Request to get current temperature
-     CurrentTemperatureRequest currentTemperatureRequest = CurrentTemperatureRequest.newBuilder()
-             .setAreaId("Room1")
-             .build();
-     CurrentTemperatureResponse currentTemperatureResponse = stub.currentTemperature(currentTemperatureRequest);
-  // Printing the response
-     System.out.println("Temperature in Room: " + currentTemperatureResponse.getTemperature());
-     System.out.println("Humidity in Room: " + currentTemperatureResponse.getHumidity());
-     System.out.println("Air Quality Index in Room: " + currentTemperatureResponse.getAirQualityIndex());
+    private static final Logger logger = Logger.getLogger(BuildingController.class.getName());
 
-     
+    public static void main (String[] args) {
 
-     
-     // Request to get HVAC condition
-  // Request to get HVAC condition
-     HVACConditionRequest hvacConditionRequest = HVACConditionRequest.newBuilder()
-             .setAreaId("Room1")
-             .build();
-     HVACConditionResponse hvacConditionResponse = stub.getHVACCondition(hvacConditionRequest);
-     // Printing the response
-     System.out.println("HVAC Status: " + hvacConditionResponse.getStatus());
-     System.out.println("Current Temperature: " + hvacConditionResponse.getCurrentTemperature());
-     System.out.println("Set Temperature: " + hvacConditionResponse.getSetTemperature());
-     System.out.println("Error Code: " + hvacConditionResponse.getErrorCode());
-	channel.shutdown();
-	
-	
+        ManagedChannel channel = ManagedChannelBuilder.forAddress ("localhost", 50051).usePlaintext().build();
+
+        BuildingServiceGrpc.BuildingServiceBlockingStub stub = BuildingServiceGrpc.newBlockingStub(channel);
+
+        AdjustTemperatureRequest request = AdjustTemperatureRequest.newBuilder().setAreaId("Room1").setTemperature(22).build();
+        AdjustTemperatureResponse response = stub.adjustTemperature(request);
+
+        //Printing adjust temperature response
+        System.out.println("Temperature adjustment was successful: " );
+        logger.info("Temperature adjustment was successful: " );
+
+        // Request to get current temperature
+        CurrentTemperatureRequest currentTemperatureRequest = CurrentTemperatureRequest.newBuilder()
+                .setAreaId("Room1")
+                .build();
+        CurrentTemperatureResponse currentTemperatureResponse = stub.currentTemperature(currentTemperatureRequest);
+        // Printing the response
+        System.out.println("Temperature in Room: " + currentTemperatureResponse.getTemperature());
+        System.out.println("Humidity in Room: " + currentTemperatureResponse.getHumidity());
+        System.out.println("Air Quality Index in Room: " + currentTemperatureResponse.getAirQualityIndex());
+
+        logger.info("Temperature in Room: " + currentTemperatureResponse.getTemperature());
+        logger.info("Humidity in Room: " + currentTemperatureResponse.getHumidity());
+        logger.info("Air Quality Index in Room: " + currentTemperatureResponse.getAirQualityIndex());
+
+        // Request to get HVAC condition
+        HVACConditionRequest hvacConditionRequest = HVACConditionRequest.newBuilder()
+                .setAreaId("Room1")
+                .build();
+        HVACConditionResponse hvacConditionResponse = stub.getHVACCondition(hvacConditionRequest);
+        // Printing the response
+        System.out.println("HVAC Status: " + hvacConditionResponse.getStatus());
+        System.out.println("Current Temperature: " + hvacConditionResponse.getCurrentTemperature());
+        System.out.println("Set Temperature: " + hvacConditionResponse.getSetTemperature());
+        System.out.println("Error Code: " + hvacConditionResponse.getErrorCode());
+
+        logger.info("HVAC Status: " + hvacConditionResponse.getStatus());
+        logger.info("Current Temperature: " + hvacConditionResponse.getCurrentTemperature());
+        logger.info("Set Temperature: " + hvacConditionResponse.getSetTemperature());
+        logger.info("Error Code: " + hvacConditionResponse.getErrorCode());
+
+        channel.shutdown();
+    }
 }
-	
-}
+
